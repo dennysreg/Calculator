6f63fa8b8b8dae51a233275e14557cbda2bbb0ad
Dennys Regalado Díaz <dennys@iMac-de-Dennys.local>
2011-11-12 13:29:57 -0600

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Dennys Regalado Díaz on 12/11/11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Dennys Regalado Díaz on 12/11/11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
